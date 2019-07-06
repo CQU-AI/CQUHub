fbc6a5316e10b6412b6d3cc2e4bf269718afe38e
1hunter0 <1292250315@qq.com>
2019-07-06 16:49:35 +0800

    搜索栏
    搜索栏无bug

--- topic/views.py
+++ topic/views.py
@@ -166,12 +166,16 @@ class Theme1_View(View):
         return render(request, 'topic_base.html', {'theme': theme1, 'theme_id': theme_id})
 
 def Go_Page(request):
-    page_id = request.POST.get('go_page')
+    try:
+        page_id = int(request.GET.get('go_page'))
+    except:
+        page_id = 1
     topic_list = Create_Topic.objects.all().order_by('-pub_time')
     paginator = Paginator(topic_list, 2)
     page_range = paginator.page_range
-    #if str(type(page_id)) != str(type(1)) and page_id > max:
-     #   page_id = 1
+    max = len(topic_list)
+    if(page_id > max):
+        page_id = 1
     pre_id = page_id-1
     next_id = page_id+1
     if page_id == 1:
@@ -179,19 +183,14 @@ def Go_Page(request):
     if page_id == len(page_range):
         next_id = page_id
         pre_id = page_id-1
-    # test=page
     try:
         topics = paginator.page(page_id)
-        # test=topics
     except  PageNotAnInteger:
         topics = paginator.page(1)
-        # test=topics
     except EmptyPage:
         topics = []
-        # paginator.page(paginator.num_pages)
-    # 处理侧边栏信息
-
     return render(request, 'topic/base.html', {'topics': topics, 'page_id': page_id, 'next_id':next_id, 'pre_id':pre_id})
+
 class TestReplywindow(View):
     def get(self, request):
         return render(request, 'topic/test_replywindow.html')
