d05e6262b0b327b8b22193fafa3b8b14d710952d
iiint <13838827815@163.com>
2019-07-06 15:16:48 +0800

    帖子组
    2019.7.6 15:16分

--- operation/models.py
+++ operation/models.py
@@ -17,3 +17,20 @@ class Topic_Comment(models.Model):
 
     def __str__(self):
         return self.content
+
+# 发帖回帖组：Topic_Reply Model，对帖子进行回复。两个外键：用户，关联评论
+
+class Topic_Reply(models.Model):
+    user = models.ForeignKey(User_Info, on_delete=models.CASCADE, verbose_name='进行回复操作的用户')
+    comment = models.ForeignKey(Topic_Comment, on_delete=models.CASCADE, verbose_name='关联评论')
+    content = models.TextField(verbose_name='评论内容')
+    add_time = models.DateTimeField(auto_now_add=True, verbose_name='评论时间')
+
+    class Meta:
+        verbose_name = '对特定评论进行回复'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.content
+
+# 发帖回帖组: class Topic_Reply END
