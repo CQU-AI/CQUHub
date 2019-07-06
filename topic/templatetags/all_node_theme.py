e3d5a88c26fca39bf92e00879da5eec3f4701c98
musenming <1045485679@qq.com>
2019-07-06 10:34:48 +0800

    Update all_node_theme.py

--- topic/templatetags/all_node_theme.py
+++ topic/templatetags/all_node_theme.py
@@ -6,12 +6,12 @@ register=template.Library()
 @register.simple_tag
 def  all_node_theme(value):
     reservedict1 = {
-        '生物知识': '1',
-        '生物信息': '2',
-        '生活交流': '3',
-        '生信编程': '4',
-        '计算机学习': '5',
-        'django学习': '6',
+        '那个谁，我想对你说': '1',
+        '动手动脚找东西': '2',
+        'CQU公告': '3',
+        'CQU身边事': '4',
+        '技术栏目': '5',
+        '文学交流': '6',
         '论坛公告': '7',
     }
 
