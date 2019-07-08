a3988572822c69a1ae7e81074dae612b5d6641df
loopyme <l-p-y@outlook.com>
2019-07-08 14:34:24 +0800

    实现邮件验证类

--- user/sender.py
+++ user/sender.py
@@ -1,58 +1,66 @@
 from email.mime.text import MIMEText
 from email.header import Header
 from smtplib import SMTP_SSL
-import time
+import time, random
 
 
-content = """
-您好:
+class Sender:
+    sender_cache = {}
 
-    感谢您使用CQU Hub！
-    为确保论坛只对重大学子开放，请确认您的学号为  {}  ，并使用验证码  {}  来完成您的注册！
-    如果您并没有在CQU Hub进行注册，请忽略此邮件。
+    def __init__(self, id, debug=False):
+        self.content = """
+        您好:
 
-顺颂 时祺
+            感谢您使用CQU Hub！
+            为确保论坛只对重大学子开放，请确认您的学号为  {}  ，并使用验证码  {}  来完成您的注册！
+            如果您并没有在CQU Hub进行注册，请忽略此邮件。
 
-CQU Hub开发组
-{}
-"""
+        顺颂 时祺
 
+        CQU Hub开发组
+        {}
+        """
+        self.student_id = id
+        self.debug = debug
 
-def generate_verify_code(student_id):
-    res = ""
-    code_map = {}
-    code = "9128405367"
-    for i in range(10):
-        code_map[str(i)] = code[i]
-    for i in str(hash(str(student_id)) % (3 ** 12)):
-        res += code_map[i]
-    print('*' * 100, '\n', res, '\n', '*' * 100)
-    return res
+    def generate_token(self):
+        token = ""
+        for i in range(6):
+            token += str(random.randint(0, 9))
+            self.sender_cache[self.student_id] = [token, time.time()]
+        return token
 
-def validate_code(userInputCode):
-    return True
+    def validate_code(self, userInputCode):
+        for id in list(self.sender_cache.keys()):
+            if self.sender_cache[id][1] - time.time() > 3600:
+                del self.sender_cache[id]
+        print(self.sender_cache)
+        return self.student_id in self.sender_cache.keys() and self.sender_cache[self.student_id][0] == userInputCode
 
-def send_verify_mail(student_id):
-    host_server = "smtp.exmail.qq.com"
-    pwd = "Djangosucks123"
-    sender_mail = "cquhub-no-reply@mail.loopy.tech"
-    receiver = "{}@cqu.edu.cn".format(student_id)
-    mail_title = "CQU Hub的注册验证"
-    mail_content = content.format(
-        student_id,
-        generate_verify_code(student_id),
-        time.strftime("%Y-%m-%d %X", time.localtime()),
-    )
+    def send_verify_mail(self):
+        if self.debug:
+            print("*" * 79 + "\n" + str(self.generate_token()) + "\n" + "*" * 79)
+            return
+        host_server = "smtp.exmail.qq.com"
+        pwd = "Djangosucks123"
+        sender_mail = "cquhub-no-reply@mail.loopy.tech"
+        receiver = "{}@cqu.edu.cn".format(self.student_id)
+        mail_title = "CQU Hub的注册验证"
+        mail_content = self.content.format(
+            self.student_id,
+            self.generate_token(),
+            time.strftime("%Y-%m-%d %X", time.localtime()),
+        )
 
-    smtp = SMTP_SSL(host_server)
+        smtp = SMTP_SSL(host_server)
 
-    smtp.set_debuglevel(0)
-    smtp.ehlo(host_server)
-    smtp.login(sender_mail, pwd)
+        smtp.set_debuglevel(0)
+        smtp.ehlo(host_server)
+        smtp.login(sender_mail, pwd)
 
-    msg = MIMEText(mail_content, "plain", "utf-8")
-    msg["Subject"] = Header(mail_title, "utf-8")
-    msg["From"] = sender_mail
-    msg["To"] = receiver
-    smtp.sendmail(sender_mail, receiver, msg.as_string())
-    smtp.quit()
+        msg = MIMEText(mail_content, "plain", "utf-8")
+        msg["Subject"] = Header(mail_title, "utf-8")
+        msg["From"] = sender_mail
+        msg["To"] = receiver
+        smtp.sendmail(sender_mail, receiver, msg.as_string())
+        smtp.quit()
