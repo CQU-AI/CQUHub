242ed60cb680f1f3f404329888b90918df557ba3
loopyme <peter@mail.loopy.tech>
2019-07-09 11:36:48 +0800

    Update settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -14,6 +14,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "keecm3wxmiefhilrhxjuhe"
+MAIL_PSW = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
