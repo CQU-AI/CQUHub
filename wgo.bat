0fca00eb2356fadf9d15277069e6521d1b88e608
musenming <1045485679@qq.com>
2019-07-09 09:50:11 +0800

    修改搜索界面直接跳转页面

--- wgo.bat
+++ wgo.bat
@@ -1,7 +1,7 @@
 rd /s /q "operation/migrations/" "user/migrations/" "topic/migrations/"
 erase db.sqlite3
-python manage.py makemigrations
-python manage.py migrate
-python manage.py migrate --run-syncdb
+py manage.py makemigrations
+py manage.py migrate
+py manage.py migrate --run-syncdb
 cls
-python manage.py runserver
+py manage.py runserver
